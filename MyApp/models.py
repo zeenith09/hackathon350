a6f8141ed2a7fb7ea35765af1bfac4f9d9c5fda2
Zenith <85917176+zeenith09@users.noreply.github.com.>
2025-04-22 13:40:09 -0500

    added models and some views

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Cart {self.id}"
+    
+    def get_total_price(self):
+        return sum(item.product.price * item.quantity for item in self.cartitem_set.all())
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from .models import Product, Cart, CartItem
 
 # Create your views here.
+
+def index(request):
+    pass
+
+def add_to_cart(request, product_id):
+    pass
+
+def remove_from_cart(request, item_id):
+    pass
+
+def view_cart(request):
+    pass
+
+def get_product_price(request, product_id):
+    pass
+
+def checkout(request):
+    pass
