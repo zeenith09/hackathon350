c6181b354ef944ea4d69e87d72d778c07d2246ba
BauerButBauerDied <bb6406yt@gmail.com>
2025-04-24 15:09:07 -0500

    add list_display for admin view of cartitems

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from MyApp.models import Product, Cart, CartItem
 
-# Register your models here.
+# Custom Classes
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("cart", "product", "quantity",)
+
+#Register models
 admin.site.register(Product)
 admin.site.register(Cart)
-admin.site.register(CartItem)
+admin.site.register(CartItem, CartItemAdmin)
