1f9255c493ad748c420931f9a115fba74d3e19b1
BauerButBauerDied <bb6406yt@gmail.com>
2025-04-24 15:10:56 -0500

    add list display for cartadmin

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -5,7 +5,10 @@ from MyApp.models import Product, Cart, CartItem
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ("cart", "product", "quantity",)
 
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at",)
+
 #Register models
 admin.site.register(Product)
-admin.site.register(Cart)
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem, CartItemAdmin)
